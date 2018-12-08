eb2da9db6d55a4a97d508d89ec5d1df9773ce4f7
tobias <tobias@pcbe15807.dyndns.cern.ch>
2018-12-08 14:09:14 +0100

    Simplified.

--- source/include/rfmulti.f90
+++ source/include/rfmulti.f90
@@ -15,66 +15,62 @@ end do
   do j=1,napx 
   
   crabamp=ed(ix)*nzz(j)
+  
   NORMAL = zero
   SKEW = zero
   PNL = zero
   PSL = zero
   
-  !nord = max(nn, ns, n_ferr/2-1)
-
 
   if(isSkew .eq. 0) then 
     if(nordm .eq. 1) then
-      print *, "are we hereeee..?"
-      pnl(0) = pi/2 - crabpase_t
-      normal(0) = crabamp/e0f
+      pnl = pi/2 - crabpase_t
+      normal = crabamp/e0f
     else
-      pnl(nordm-1) =  -crabpase_t
-      normal(nordm-1) = -crabamp*fact
+      pnl =  -crabpase_t
+      normal = -crabamp*fact
     endif
   else
     if(nordm .eq. 1) then
-      psl(0) = -pi/2 - crabpase_t
-      skew(0) = crabamp/e0f
+      psl = -pi/2 - crabpase_t
+      skew = crabamp/e0f
     else
-      psl(nordm-1) = - crabpase_t
-      skew(nordm-1) = crabamp*fact
+      psl = - crabpase_t
+      skew = crabamp*fact
     endif
   endif
+
   krf = (((one/(clight*(e0f/e0)))*crabfreq)*two)*pi
   
 
 
-  print * , "krf",  "crabfreq", "crabamp",  "phase", pnl(0), normal(0)
-  print * , krf, crabfreq, crabamp, crabph(ix), pnl(1), normal(1), "ddddd"
-  print *, psl(1), skew(1), "skeeewww11"
+  print * , "krf",  "crabfreq", "crabamp",  "phase", pnl, normal
+  print * , krf, crabfreq, crabamp, crabph(ix), pnl, normal, "ddddd"
+  print *, psl, skew, "skeeewww11"
 
-    ! if zero:th order prepare
-    ! if not zero change phase with a minus sign and no multiply
-  !---- Prepare to calculate the kick and the matrix elements
+
     x_t = xv1(j)*c1m3 
     y_t = xv2(j)*c1m3
-    !---- Vector with strengths 
-    do iord = 0, nordm
-      print *,"skewww",   krf, skew(0), psl(0)
-      field_cos(1,iord) = (normal(iord) * cos(pnl(iord)  - krf * sigmv(j)))
-      field_sin(1,iord) = (normal(iord) * sin(pnl(iord)  - krf * sigmv(j)))
-      field_cos(2,iord) = (skew(iord)   * cos(psl(iord)  - krf * sigmv(j)))
-      field_sin(2,iord) = (skew(iord)   * sin(psl(iord)  - krf * sigmv(j)))
-    enddo
-    print *, "is fieeeld", field_cos(2,0), field_sin(2,0), nord
+
+      field_cos(1) = (normal * cos(pnl  - krf * sigmv(j)))
+      field_sin(1) = (normal * sin(pnl  - krf * sigmv(j)))
+      field_cos(2) = (skew   * cos(psl  - krf * sigmv(j)))
+      field_sin(2) = (skew   * sin(psl  - krf * sigmv(j)))
+   
+
     Cm2 = zero; Sm2a = zero; Cm1 = zero; Sm1a = zero;
     Cp0 = zero; Sp0 = zero; Cp1 = zero; Sp1 = zero;
 
-    do iord = nordm, 0, -1
-    print * , "iiooord", iord, nordm
-
-      Cp0 = Cp0 * (x_t+imag*y_t) / (iord+1)   + field_cos(1,iord)+imag*field_cos(2,iord);
-      Sp1 = Sp1 * (x_t+imag*y_t) / (iord+2)   + field_sin(1,iord)+imag*field_sin(2,iord);
-      !Cp0 = Cp0 * (x_t+imag*y_t) / (iord+1)  ! + field_cos(1,iord)+imag*field_cos(2,iord);
-      !Sp1 = Sp1 * (x_t+imag*y_t) / (iord+2)  ! + field_sin(1,iord)+imag*field_sin(2,iord);
-
-      
+    do iord = nordm-1, 0, -1
+      if(iord .eq. nordm-1) then
+        Cp0 = field_cos(1)+imag*field_cos(2);
+        Sp1 = field_sin(1)+imag*field_sin(2);
+      else 
+        Cp0 = Cp0 * (x_t+imag*y_t) / (iord+1)  
+        Sp1 = Sp1 * (x_t+imag*y_t) / (iord+2) 
+        print *, "ooord",  (iord+1)
+        print *, "ooorf2",  (iord+2)
+      endif
     enddo
     Sp1 = Sp1 * (x_t+imag*y_t);
     
@@ -82,9 +78,10 @@ end do
     dpx = -REAL(Cp0)*c1e3*moidpsv(j);
     dpy = AIMAG(Cp0)*c1e3*moidpsv(j);
     dpt = - krf * REAL(Sp1)*c1e3*e0f;
+    
     yv1(j) = yv1(j) + dpx
     yv2(j) = yv2(j) + dpy
-    ejv(j)=ejv(j)+dpt
+    ejv(j) = ejv(j) + dpt
     
 
     ejf0v(j)=ejfv(j)
